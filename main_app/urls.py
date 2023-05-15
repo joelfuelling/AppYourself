6c2299d49a63ded6f8584bbd0365f6631f46b01c
Joel <JoelFuelling@gmail.com>
2023-05-15 17:57:51 -0500

    Rough afternoon of bugs and getting sick. CreateView class function setup and working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('jobs/', views.jobs_index, name='index'),
+    path('roles/', views.RoleList.as_view(), name='index'),
+    path('roles/create', views.RoleCreate.as_view(), name='roles_create'),
+    path('roles/<int:pk>', views.roles_detail, name='role_detail'),
 ]

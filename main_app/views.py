44c0a86af3a8ba951d71ac4e83ce77442c3d7ab6
Joel <JoelFuelling@gmail.com>
2023-05-15 00:01:02 -0500

    environment is working

--- main_app/views.py
+++ main_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render (request, 'home.html')
 # Create your views here.

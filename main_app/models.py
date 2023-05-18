05587665b5034fb5abae31e83371d651eaeb5d8a
Joel <JoelFuelling@gmail.com>
2023-05-18 15:33:55 -0500

    no changes

--- main_app/models.py
+++ main_app/models.py
@@ -19,7 +19,7 @@ class Role(models.Model):
     company_name = models.CharField(max_length=100)
     salary = models.CharField(default='$', max_length=100)
     location = models.CharField(max_length=100)
-    description = models.TextField(default='(include a link!)')
+    description = models.TextField(default='(include a link!)', max_length=3000)
     pub_date = models.DateField('date added') #* Default set to today in static JS file.
     tags = models.ManyToManyField(Tag)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
